394c107cb3dce928c07417092fd473548981f5fd
Nourelden Rihan <noureldenrihan@gmail.com>
2023-03-26 00:06:03 +0200

    Finished Vertical Navbar.

--- src/VerticalNavbar/VerticaNavbar.jsx
+++ src/VerticalNavbar/VerticaNavbar.jsx
@@ -1,7 +1,45 @@
 import "./VerticalNavbar.css";
 
 function VerticalNavbar() {
-  return <div>Vert</div>;
+  function choose(e) {
+    [...document.getElementsByClassName("navTab")].forEach((element) => {
+      if (element.classList.contains("tab")) {
+        element.className = element.className.slice(0, -4);
+      }
+      if (!element.classList.contains("tabIcon")) {
+        element.className += " tabIcon";
+      }
+    });
+
+    if (!e.target.classList.contains("tab")) {
+      let fillclass = e.target.className.slice(0, -7) + " tab";
+      e.target.className = fillclass;
+    }
+  }
+
+  return (
+    <div className="VerticalNavbar">
+      <div className="profileIcon">
+        <img src="/Images/Nourelden Rihan Profile Pic.jpg" alt="User" />
+      </div>
+      <div className="tabs">
+        <i class="Icon bi bi-house navTab tabIcon tab" onClick={choose}></i>
+        <i class="Icon bi bi-flower1 navTab tabIcon tab" onClick={choose}></i>
+        <i
+          class="Icon bi bi-shop-window navTab tabIcon tab"
+          onClick={choose}
+        ></i>
+        <i
+          class="Icon bi bi-controller navTab tabIcon tab"
+          onClick={choose}
+        ></i>
+      </div>
+      <div className="advanced">
+        <i class="Icon bi bi-upload"></i>
+        <i class="Icon bi bi-gear"></i>
+      </div>
+    </div>
+  );
 }
 
 export default VerticalNavbar;
